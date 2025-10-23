db709fa61cee336baab122b3c8245c50f137bb61
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-10-23 17:25:54 +0900

    fv_second

---
+++ src/components/SecondView/SecondView.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styles from "./SecondView.module.css";
+import Image from 'next/image';
+
+export default function SecondView() {
+    return (
+        <>
+            <section className={styles.second_view}>
+                    <Image
+                        src="/images/fv_second/back.jpg"
+                        alt="店内の写真"
+                        className={styles.second_back}
+                        fill
+                    />
+                    <div className={styles.container}>
+                        <h2>
+                            都会のオアシス<br />
+                            カフェ・リヴィエラで<br />
+                            心安らぐひとときを
+                        </h2>
+                        <p>
+                            私たちのカフェは、日々の忙しさを忘れ、<br />
+                            ゆったりとした時間を楽しむ場所です。<br />
+                            洗練された空間で、美味しいコーヒーと共に<br />
+                            リラックスしてください。
+                        </p>
+                        <div className={styles.album}>
+                            <Image
+                                className={styles.album_item}
+                                src="/images/fv_second/img1.jpeg"
+                                alt=''
+                                width={165}
+                                height={200}
+                            />
+                            <Image
+                                className={styles.album_item}
+                                src="/images/fv_second/img2.jpeg"
+                                alt=''
+                                width={165}
+                                height={200}
+                            />
+                            <Image
+                                className={styles.album_item}
+                                src="/images/fv_second/img3.jpeg"
+                                alt=''
+                                width={165}
+                                height={200}
+                            />
+                            <Image
+                                className={styles.album_item}
+                                src="/images/fv_second/img4.jpeg"
+                                alt=''
+                                width={165}
+                                height={200}
+                            />
+                            <Image
+                                className={styles.album_item}
+                                src="/images/fv_second/img5.jpeg"
+                                alt=''
+                                width={165}
+                                height={200}
+                            />
+                            <Image
+                                className={styles.album_item}
+                                src="/images/fv_second/img6.jpeg"
+                                alt=''
+                                width={165}
+                                height={200}
+                            />
+                        </div>
+                    </div>
+            </section>
+        </>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import FirstView from "@/components/FirstView/FirstView";
 import Mobile from "@/components/Mobile/Mobile";
+import SecondView from "@/components/SecondView/SecondView";
 
 export default function Home() {
   return (
@@ -12,6 +11,7 @@ export default function Home() {
           <div className={styles.inner}>
             <Mobile>
               <FirstView />
+              <SecondView />
             </Mobile>
           </div>
         </div>
